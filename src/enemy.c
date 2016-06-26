8388f7842554a62a0347c6f8390c8e712f39d66c
vitorvanacor <vitor_vanacor@hotmail.com>
2016-06-26 15:38:39 -0300

    IA

--- src/enemy.c
+++ src/enemy.c
@@ -1,6 +1,7 @@
 typedef struct ENEMY {
     GLMmodel *model;
     int id;
+    bool isDead;
     float x;
     float y;
     float z;
@@ -17,25 +18,31 @@ typedef struct ENEMY {
 
 void newEnemies();
 Enemy newEnemy(int id);
-void EnemyUpdateAll();
-void EnemyUpdate(Enemy* e);
+void EnemyUpdateAll(Position pPos);
+void EnemyUpdate(Enemy* e, Position pPos);
 void EnemyDrawAll();
 void EnemyDraw(Enemy e);
 void setEnemyPositions(Enemy* e);
 void EnemyShove(Enemy* e);
 void EnemyTurn(Enemy* e);
 void EnemyMove(Enemy* e);
-void EnemyDecideAction(Enemy* e);
+void EnemyDecideAction(Enemy* e, Position pPos);
 Position getEnemyPosition(Enemy e);
 bool EnemyEnemyCollision(float x, float z, int id);
 void EnemyFall(Enemy* e);
 void loadModel(Enemy* e);
+bool EnemyCanMoveTo(float x, float z, int id);
+void EnemyChasePlayer(Enemy* e, Position pPos);
+void EnemyTurnRandom(Enemy* e);
+double sinD(float radAngle);
+double cosD(float radAngle);
 
-float enemyMoveSpeed = 0.05;
-float enemyTurnSpeed = 30; //Deve ser divisor de 90
+float enemyMoveSpeed = 0.025;
+float enemyTurnSpeed = 45; //Deve ser divisor de 90
 float enemyRadius = 0.1;
-float enemyShoveSpeed = 0.5;
-float enemyShoveDeceleration = 0.005;
+float enemyShoveSpeed = 0.1;
+float enemyShoveDeceleration = 0.01;
+int aliveEnemies = 0;
 Enemy* enemies;
 
 void newEnemies(){
@@ -50,6 +57,7 @@ Enemy newEnemy(int id){
     printf("New Enemy.\n");
     Enemy e;
     e.id = id;
+    e.isDead = false;
     e.x = 0.0f;
     e.y = 0.0f;
     e.z = 2.0f;
@@ -67,13 +75,14 @@ Enemy newEnemy(int id){
     e.turningRight = false;
 
     loadModel(&e);
+    aliveEnemies = m.numEnemies;
 
     return e;
 }
 
 void loadModel(Enemy* e) {
 
-    e->model = glmReadOBJ("../../res/ladybird.obj");
+    e->model = glmReadOBJ("../../res/models/enemy.obj");
 
     glmUnitize(e->model);
     glmScale(e->model,0.12); // USED TO SCALE THE OBJECT
@@ -81,25 +90,25 @@ void loadModel(Enemy* e) {
     glmVertexNormals(e->model, 90.0);
 }
 
-void EnemyUpdateAll(){
+void EnemyUpdateAll(Position pPos){
     int i;
     for (i = 0; i < m.numEnemies; i++)
-        EnemyUpdate(&enemies[i]);
+        EnemyUpdate(&enemies[i], pPos);
 }
 
-void EnemyUpdate(Enemy* e) {
-    if (getTileXZ(e->x, e->z) == EMPTY){
+void EnemyUpdate(Enemy* e, Position pPos) {
+    if (getTileXZ(e->x, e->z) == EMPTY)
         EnemyFall(e);
+    if (e->shoveSpeedX != 0 || e->shoveSpeedZ != 0){
+        EnemyShove(e);
         return;
     }
-    EnemyDecideAction(e);
-    if (e->shoveSpeedX != 0 || e->shoveSpeedZ != 0)
-        EnemyShove(e);
     if (e->turningRight ^ e->turningLeft) {
         EnemyTurn(e);
-    } else {
-        EnemyMove(e);
+        return;
     }
+    EnemyDecideAction(e, pPos);
+    EnemyMove(e);
 }
 
 void EnemyShove(Enemy* e) {
@@ -128,7 +137,7 @@ void EnemyShove(Enemy* e) {
 }
 
 void EnemyTurn(Enemy* e){
-    int direction = e->turningRight - e->turningLeft;
+    int direction = e->turningLeft - e->turningRight;
     e->roty += enemyTurnSpeed*direction;
     if (e->roty % 90 == 0){
         e->turningRight = false;
@@ -142,19 +151,22 @@ void EnemyTurn(Enemy* e){
 }
 
 void EnemyMove(Enemy* e){
-    e->speedX = -enemyMoveSpeed * sin(e->roty*PI/180);
-    e->speedZ = -enemyMoveSpeed * cos(e->roty*PI/180);
+    e->speedX = enemyMoveSpeed * cosD(e->roty);
+    e->speedZ = -enemyMoveSpeed * sinD(e->roty);
     float newX = e->x + e->speedX;
     float newZ = e->z + e->speedZ;
-    if (!hasTypeAt(newX, newZ, enemyRadius, BLOCK) &&
-        !hasTypeAt(newX, newZ, enemyRadius, EMPTY) &&
-        !EnemyEnemyCollision(newX, newZ, e->id)){
-            e->x = newX;
-            e->z = newZ;
+    if (EnemyCanMoveTo(newX, newZ, e->id)){
+        e->x = newX;
+        e->z = newZ;
     } else {
         e->turningRight = true;
-        e->lastTurnTime = time(NULL);
     }
+}
+
+bool EnemyCanMoveTo(float x, float z, int id){
+    return (!hasTypeAt(x, z, enemyRadius, BLOCK) &&
+            !hasTypeAt(x, z, enemyRadius, EMPTY) &&
+            !EnemyEnemyCollision(x, z, id));
 }
 
 void EnemyFall(Enemy* e){
@@ -168,7 +180,11 @@ void EnemyFall(Enemy* e){
         e->speedY += gravity;
         e->y -= e->speedY;
     }
-
+    if (!e->isDead){
+        e->isDead = true;
+        aliveEnemies--;
+        printf("Alive: %d\n",aliveEnemies);
+    }
 }
 
 bool EnemyEnemyCollision(float x, float z, int id){
@@ -178,23 +194,66 @@ bool EnemyEnemyCollision(float x, float z, int id){
         e = enemies[i];
         if (x-enemyRadius < e.x+enemyRadius && x+enemyRadius > e.x-enemyRadius &&
             z-enemyRadius < e.z+enemyRadius && e.z+enemyRadius > e.z-enemyRadius
-            && e.y > -1)
+            && !e.isDead)
                 if (e.id != id) return true;
     }
     return false;
 }
 
-void EnemyDecideAction(Enemy* e){
-    if (e->shoveSpeedX != 0 || e->shoveSpeedZ != 0)
-        return;
-    if (time(NULL) - e->lastTurnTime > e->walkingTime) {
-        e->lastTurnTime = time(NULL);
-        if (rand() % 2 == 0)
-            e->turningLeft = true;
-        else
-            e->turningRight = true;
-        e->walkingTime = 99999; //rand() % 3 + 1;
+void EnemyDecideAction(Enemy* e, Position pPos){
+    Position ePos = getPositionXZ(e->x, e->z);
+    if (abs(pPos.x - ePos.x) <= 4 || abs(pPos.z - ePos.z) <= 4){ //Se está dentro do raio de visao
+        EnemyChasePlayer(e, pPos);
     }
+    else if (time(NULL) - e->lastTurnTime > e->walkingTime) {
+        printf("VIRADA ALEATORIA\n");
+        EnemyTurnRandom(e);
+        e->lastTurnTime = time(NULL);
+        e->walkingTime = rand() % 3 + 1;
+    }
+}
+
+void EnemyChasePlayer(Enemy* e, Position pPos){
+    //printf("PPERSEGUINDO\n");
+    Position ePos = getPositionXZ(e->x, e->z);
+    if(abs(pPos.x-ePos.x)>abs(pPos.z-ePos.z)){ //Se a distancia X for maior
+        if (pPos.x>ePos.x){ //Se o x do jogador é maior
+            switch(e->roty){
+            case E: break; // Está virado na direção certa
+            case N: e->turningRight = true; break;
+            case W: EnemyTurnRandom(e); break;
+            case S: e->turningLeft = true; break;
+            }
+        } else{ // x do inimigo é maior
+            switch(e->roty){
+            case W: break; // Está virado na direção certa
+            case S: e->turningRight = true; break;
+            case E: EnemyTurnRandom(e); break;
+            case N: e->turningLeft = true; break;
+            }
+        }
+    } else{ //Distancia Z é maior
+        if (pPos.z>ePos.z){ //Se o z do jogador é maior
+            switch(e->roty){
+            case S: break; // Está virado na direção certa
+            case E: e->turningRight = true; break;
+            case N: EnemyTurnRandom(e); break;
+            case W: e->turningLeft = true; break;
+            }
+        } else{ // z do inimigo é maior
+            switch(e->roty){
+            case N: break; // Está virado na direção certa
+            case W: e->turningRight = true; break;
+            case S: EnemyTurnRandom(e); break;
+            case E: e->turningLeft = true; break;
+            }
+        }
+    }
+}
+
+void EnemyTurnRandom(Enemy* e){
+    if (rand() % 2 == 0) e->turningLeft = true;
+    else e->turningRight = true;
 }
 
 void EnemyDrawAll(){
@@ -209,7 +268,7 @@ void EnemyDraw(Enemy enemy) {
         float eyeY = enemy.y + 0.119;
 
         glTranslatef(GLfloat(enemy.x), GLfloat(eyeY), GLfloat(enemy.z));
-        glRotatef(enemy.roty, 0, 1, 0);
+        glRotatef(enemy.roty+90, 0, 1, 0);
         glmDraw(enemy.model, GLM_SMOOTH);
     glPopMatrix();
 }
@@ -222,7 +281,6 @@ Position getEnemyPosition(Enemy e){
 }
 
 void setEnemyPositions(Enemy* e) {
-    Map m = cloneMap();
     int index = 0, i, j;
     for (i = 0; i < m.width; i++)
        for (j = 0; j < m.width; j++)
@@ -231,4 +289,10 @@ void setEnemyPositions(Enemy* e) {
                (e+index)->z = j * m.tileSize + m.tileSize/2.0;
                index++;
            }
+}
+double sinD(float radAngle){
+    return sin(radAngle*PI/180.0);
+}
+double cosD(float radAngle){
+    return cos(radAngle*PI/180.0);
 }
